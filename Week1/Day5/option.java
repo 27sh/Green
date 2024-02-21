47fc0d0d393961489d6ca238f54623f13db00b56
27sh <120109008+27sh@users.noreply.github.com>
2024-02-21 14:54:23 +0900

    Create option.java

---
+++ Week1/Day5/option.java
@@ -0,0 +1,23 @@
+package day01;
+
+public class Option {
+	
+	public int n1;
+	public int n2;
+	
+	public int add (int n1, int n2) {
+		return n1 + n2;
+	}
+	
+	public int dev (int n1, int n2) {
+		return n1 - n2;
+	}
+	
+	public int mul (int n1, int n2) {
+		return n1 * n2;
+	}
+	
+	public float div (float n1, float n2) {
+		return n1 / n2;
+	}
+}
