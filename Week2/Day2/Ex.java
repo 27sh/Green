21ce7ac98a6105d743166526bc0b33078ce2afe9
27sh <120109008+27sh@users.noreply.github.com>
2024-02-23 11:51:36 +0900

    Create Ex.java

---
+++ Week2/Day2/Ex.java
@@ -0,0 +1,17 @@
+package day01;
+
+public class Ex {
+
+	public static void main(String[] args) {
+
+		int n = 2;
+		int m = 5;
+		
+		F1 f1 = new F1();
+		
+		int re3 = f1.mul(n, m);
+		
+		System.out.println(re3);
+	}
+
+}
