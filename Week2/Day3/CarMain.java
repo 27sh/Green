4b8b1d18ad3e238ef2aa09f176fbd0bd3b975771
27sh <120109008+27sh@users.noreply.github.com>
2024-02-26 10:47:51 +0900

    Create CarMain.java

---
+++ Week2/Day3/CarMain.java
@@ -0,0 +1,17 @@
+package day03;
+
+public class CarMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Car car1 = new Car();
+		Car car2 = new Car();
+		Car car3 = new Car();
+		
+		System.out.println(car1.getProdNo());
+		System.out.println(car2.getProdNo());
+		System.out.println(car3.getProdNo());
+		System.out.println(Car.count);
+	}
+
+}
