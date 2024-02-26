9f96093e143f29f4990b2cc71bc1ca3f2b5db021
27sh <120109008+27sh@users.noreply.github.com>
2024-02-26 15:20:39 +0900

    Update Arr2.java

--- Week2/Day3/Arr2.java
+++ Week2/Day3/Arr2.java
@@ -6,13 +6,15 @@ public class Arr2 {
 		// TODO Auto-generated method stub
 		int i = 5;
 		int m = 3;
+		int cnt = 1;
 		
 		int[][] arr = new int[i][m];
 	
 		for(int k = 0; k < i; k++) {
 			for(int w = 0; w < m; w++) {
-				arr[k][w] = 1;
-				System.out.print(arr[k][w]);
+				arr[k][w] = cnt;
+				cnt++;
+				System.out.print(arr[k][w] + " ");
 			}
 			System.out.println();
 		}
