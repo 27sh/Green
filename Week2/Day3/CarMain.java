40212f0b1097c823b72803e7a841963f97cc2ddd
27sh <120109008+27sh@users.noreply.github.com>
2024-02-26 11:10:15 +0900

    Update CarMain.java

--- Week2/Day3/CarMain.java
+++ Week2/Day3/CarMain.java
@@ -4,6 +4,8 @@ public class CarMain {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Car.f();
+
 		Car car1 = new Car();
 		Car car2 = new Car();
 		Car car3 = new Car();
