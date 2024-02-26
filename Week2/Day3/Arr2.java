909876133df4f0fc8cb71a790e3a792ba66e8434
27sh <120109008+27sh@users.noreply.github.com>
2024-02-26 15:19:05 +0900

    Create Arr2.java

---
+++ Week2/Day3/Arr2.java
@@ -0,0 +1,23 @@
+package day03;
+
+public class Arr2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i = 5;
+		int m = 3;
+		
+		int[][] arr = new int[i][m];
+	
+		for(int k = 0; k < i; k++) {
+			for(int w = 0; w < m; w++) {
+				arr[k][w] = 1;
+				System.out.print(arr[k][w]);
+			}
+			System.out.println();
+		}
+		
+	}
+	
+
+}
