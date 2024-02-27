7c8b2b814da054a0a2a8f0c1b730b63a5bbe0f7d
27sh <120109008+27sh@users.noreply.github.com>
2024-02-27 12:49:41 +0900

    Create UnitTest.java

---
+++ Week2/Day4/UnitTest.java
@@ -0,0 +1,15 @@
+package day04;
+
+public class UnitTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		UnitA a = new UnitA("A");
+		UnitB b = new UnitB("B");
+		
+		b.attack(a);
+		
+		System.out.println(a);
+	}
+
+}
