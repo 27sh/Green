d0768ca2660c0fdc87f0a9bf334dc3c257f424bf
27sh <120109008+27sh@users.noreply.github.com>
2024-02-29 17:16:45 +0900

    Create ManageTell.java

---
+++ Week3/Day1/ManageTell.java
@@ -0,0 +1,62 @@
+package day06;
+
+public class ManageTell {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Member[] m = new Member[3];
+		
+		m[0] = new Member("홍길동", "010-1111-2222", "M");
+		m[1] = new Member("임꺽정", "010-1234-5678", "M");
+		m[2] = new Member("일지매", "010-9999-8888", "F");
+		
+		for(Member i : m) {
+			System.out.println(i.toString());
+		}
+		
+	}
+
+}
+package day06;
+
+public class Member {
+	private String name;
+	private String tell;
+	private String gender;
+	
+	Member(){}
+	
+	Member(String name, String tell, String gender){
+		this.name = name;
+		this.tell = tell;
+		this.gender = gender;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getTell() {
+		return tell;
+	}
+	public void setTell(String tell) {
+		this.tell = tell;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return "이름 : " +name+ ", 전화번호 : " +tell+ ", 성별 : " +gender;
+	}
+
+}
+
+
