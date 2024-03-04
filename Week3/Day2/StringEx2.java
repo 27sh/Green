0100854eb836f28553344678084034c4352ca6ee
27sh <120109008+27sh@users.noreply.github.com>
2024-03-04 15:17:22 +0900

    Create StringEx2.java

---
+++ Week3/Day2/StringEx2.java
@@ -0,0 +1,18 @@
+package interfaceEx;
+
+public class StringEx2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String str = "I am a boy";
+		char[] c = str.toCharArray();
+		
+		for(char a : c) {
+			System.out.println(a);
+		}
+		
+		
+		
+	}
+
+}
