3dd33770a8b1141ac4683c971c996112a6ae005f
27sh <120109008+27sh@users.noreply.github.com>
2024-03-13 17:46:28 +0900

    Create UI.java

---
+++ Week4/Day5/UI.java
@@ -0,0 +1,20 @@
+package project2;
+
+public class UI {
+	public void main() {
+		System.out.println("* - * - * - *");
+		System.out.println("  입출고 관리   ");
+		System.out.println("* - * - * - *");
+	}
+	
+	public void menu() {
+		System.out.println("수행하려는 작업을 선택해주세요.");
+		
+		System.out.println();
+		
+		System.out.println("1. 입고");
+		System.out.println("2. 출고");
+		System.out.println("3. 재고 확인");
+		System.out.println("4. 프로그램 종료");
+	}
+}
