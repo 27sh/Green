55351a9fddd70b7f48b61e727d8c541a73a27b18
27sh <120109008+27sh@users.noreply.github.com>
2024-03-22 13:39:28 +0900

    Create FileInputStreamTest2.java

---
+++ Week6/DAY2/io/FileInputStreamTest2.java
@@ -0,0 +1,37 @@
+package io;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class FileInputStreamTest2 {
+
+	public static void main(String[] args) {
+		int inputData = 0;
+		
+		FileInputStream fls = null;
+		
+		try {
+			fls = new FileInputStream("Sample.txt");
+			
+			while((inputData = fls.read()) != -1) {
+				System.out.print((char)inputData);
+			}
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		finally {
+			try {
+				fls.close();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+
+}
