554456b0bf45dea9c71e0d23716e17a2bc15ee25
27sh <120109008+27sh@users.noreply.github.com>
2024-03-26 14:03:41 +0900

    Create NestedClassTest.java

---
+++ Week6/DAY4/NestedClassTest.java
@@ -0,0 +1,14 @@
+
+public class NestedClassTest {
+
+	public static void main(String[] args) {
+		Outer outer = new Outer();
+		
+		/*
+		Outer.Inner inner = outer.new Inner();
+		
+		inner.myMethod();
+		*/
+	}
+
+}
